0670297139833983d56a67bdcd65ec57d9e62a5d
Fujita-Nozomi <Nishikino-Maki@openmailbox.org>
2017-04-18 05:35:32 +0800

    Java Version

---
+++ majiyabakune.java
@@ -0,0 +1,11 @@
+public class majiyabakune
+{
+    public static void main(String[] args)throws Exception
+    {
+        for(;;)
+        {
+            System.out.println("マジやばくね...");
+            Thread.sleep(500);
+        }
+    }
+}
