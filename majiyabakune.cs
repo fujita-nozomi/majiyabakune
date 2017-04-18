46b9f4d5b71cbaadb5ad25e248fd453d90840354
Fujita-Nozomi <Nishikino-Maki@openmailbox.org>
2017-04-19 03:23:07 +0800

    C# version

---
+++ majiyabakune.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+namespace majiyabakune
+{
+    class majiyabakune
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1;i <= 10;)
+            {
+                Console.WriteLine("マジやばくね...");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
